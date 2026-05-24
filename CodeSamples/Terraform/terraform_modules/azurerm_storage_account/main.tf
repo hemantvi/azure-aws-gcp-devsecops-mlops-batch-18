4f4d76958d8f293bdf57f1872e701efd30e385fd
DevOps Insiders <info@devopsinsiders.com>
2026-05-24 23:45:23 +0530

    Add Terraform modules for Azure resources
    - Created a module for Azure Resource Group with multiple resource groups defined.
    - Added a module for Azure Storage Account with variable configurations for multiple storage accounts.
    - Implemented a module for Azure Subnet with variable configurations for multiple subnets.
    - Developed a module for Azure Virtual Network with variable configurations for multiple virtual networks.
    - Included provider configurations for Azure in each module.
    - Added Terraform variable files for each module to manage input variables.

--- CodeSamples/Terraform/terraform_modules/azurerm_storage_account/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_storage_account" "example" {
-  for_each = var.storage_accounts
-
-  name                     = each.value.stg_name
-  resource_group_name      = each.value.rg_name
-  location                 = each.value.location
-  account_tier             = each.value.tier
-  account_replication_type = each.value.redundancy
-}
